4729f020462197655d0a99b7f244bccb19a1b359
이은정 <ej2grateful@gmail.com>
2025-06-09 14:04:38 +0900

    substring

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,9 @@ import java.util.Arrays;
 public class Main {
 
      public static void main(String[] args) {
-         int[] numbers = { 0, 1, 2, 3, };
-         System.out.println("numbers 길이 : " + numbers.length);
+         String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
 
-         String message = "Hello, Java!";
-
-         System.out.println(message);
-         System.out.println("길이 : " + message.length());
-         System.out.println("대문자 변환 : " + message.toUpperCase());
-         System.out.println("소문자 변환 : " + message.toLowerCase());
-
-         System.out.println("순서 : " + message.indexOf("l"));
-
-         for(int i = 0; i < message.length(); i++) {
-             System.out.println(message.charAt(i));
-             if(message.charAt(i) == 'l' ) {
-                 System.out.println("순서2 : " + i);
-                 break;
-             }
-         }
+         System.out.println(address.substring(address.length() - 5));
 
     }
 }
